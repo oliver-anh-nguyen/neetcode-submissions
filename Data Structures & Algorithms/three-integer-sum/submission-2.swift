d233c3243dcab87272a2d9b69183c6e3c2ba26bf
Oliver Nguyen <45956958+oliver-anh-nguyen@users.noreply.github.com>
2026-07-01 15:25:44 -0500

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.swift
@@ -0,0 +1,32 @@
+class Solution {
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        var res = [[Int]]()
+        var nums = nums.sorted()
+        for i in 0..<nums.count {
+            let a = nums[i]
+            if a > 0 {
+                break
+            }
+            if i > 0 && a == nums[i-1] {
+                continue
+            }
+            var l = i + 1, r = nums.count - 1
+            while l < r {
+                let threeSum = a + nums[l] + nums[r]
+                if threeSum < 0 {
+                    l += 1 
+                } else if threeSum > 0 {
+                    r -= 1
+                } else {
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r && nums[l] == nums[l-1] {
+                        l += 1
+                    }
+                }
+            }
+        }
+        return res
+    }
+}
