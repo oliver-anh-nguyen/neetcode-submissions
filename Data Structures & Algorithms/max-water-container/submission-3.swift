9c50679d5f6dbcb218760383ce4ad9354c5b6d33
Oliver Nguyen <45956958+oliver-anh-nguyen@users.noreply.github.com>
2026-07-06 21:41:09 -0500

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func maxArea(_ heights: [Int]) -> Int {
+        var l = 0, r = heights.count - 1
+        var res = 0
+
+        while l < r {
+            let area = min(heights[l], heights[r]) * (r-l)
+            res = max(area, res)
+            if heights[l] < heights[r] {
+                l += 1
+            } else {
+                r -= 1
+            }
+        }
+        return res
+    }
+}
